8bf77d500354bd6b87930765cb70b2717d66e25a
jhanvi14 <57310906+jhanvi14@users.noreply.github.com>
2019-12-01 20:23:18 +0530

    Add files via upload

---
+++ Balanced.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Balanced {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter String in form of Parenthesis");
+        String str=in.next();
+        Stack s=new Stack();
+        int d=0,j=0;
+        char arr[]=str.toCharArray();
+        int n=arr.length;
+        s.push('(');
+        s.push(arr[0]);
+        for(int i=1;i<n;i++)
+        {
+            s.push(arr[i]);
+            j=s.size()-2;
+            char c=(char)s.peek();
+            d=c-(char)s.get(j);
+            if(d>=1 && d<=2)
+            {
+                s.pop();
+                s.pop();
+            }
+        }
+        if(s.size()==1)
+            System.out.println("Balanced");
+        else
+            System.out.println("Not Balanced");
+    }
+}
